f822c60ca64e84c4c825a475f066a6c5827344ea
Mohammad Nayeem Hasan <133757680+smnhasan@users.noreply.github.com>
2024-03-28 11:08:34 +0600

    Add files via upload

---
+++ DengueWorld2023.R
@@ -0,0 +1,141 @@
+library(MASS)
+require(foreign)
+require(ggplot2)
+require(maptools)
+library(tidyverse)
+library(betareg)
+library(car)
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+library(dplyr)
+library(patchwork) # To display 2 charts together
+library(hrbrthemes)
+library(psych) # for descriptive analysis
+library(forecast)
+library(lmtest)
+library(tseries)
+
+##Descriptive
+##WORLD SUMMARY
+
+#MAP EM
+
+setwd("E:\\ResearchProject\\Najmul Bhai\\Dengue\\Dengue World 2023")
+
+worldDeng <- read.csv("DengWorld.csv")
+worldDeng$Caselog <- log10(worldDeng$Case)
+worldDeng$Deathlog <- log10(worldDeng$Death)
+
+# COVID2022$location[COVID2022$location == 'United States'] <- 'USA'
+# COVID2022$location[COVID2022$location == 'United Kingdom'] <- 'UK'
+# COVID2022$location[COVID2022$location == 'Democratic Republic of Congo'] <- 'Democratic Republic of the Congo'
+# COVID2022$location[COVID2022$location == 'Congo'] <- 'Republic of Congo'
+
+
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(maps)
+
+options(scipen = 999) ## To disable scientific notation
+world <- map_data("world")
+worldplot <- ggplot() +
+  geom_polygon(data = world, aes(x=long, y = lat, group = group)) + 
+  coord_fixed(1.3)
+worldplot
+
+
+worldgovt <- dplyr::select(worldDeng, region = Country, DC = Caselog)
+head(worldgovt)
+
+
+## Make the HDI numeric
+worldgovt$DC <- as.numeric(as.character(worldgovt$DC))
+worldSubset <- inner_join(world, worldgovt, by = "region")
+head(worldSubset)
+
+
+## Let's ditch many of the unnecessary elements
+plain <- theme(
+  axis.text = element_blank(),
+  axis.line = element_blank(),
+  axis.ticks = element_blank(),
+  panel.border = element_blank(),
+  panel.grid = element_blank(),
+  axis.title = element_blank(),
+  panel.background = element_rect(fill = "white"),
+  plot.title = element_text(hjust = 0.5)
+)
+
+worldDeng <- ggplot(data = worldSubset, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) +
+  geom_polygon(aes(fill = DC)) +
+  scale_fill_distiller(palette ="YlGn", direction = 1) + # or direction=1
+  ggtitle("") + labs(fill = "Dengue Cases \n(log10)") +
+  plain
+x <- plot(worldDeng)
+x
+
+
+#Death
+
+worldDeng <- read.csv("DengWorld.csv")
+worldDeng$Caselog <- log10(worldDeng$Case)
+worldDeng$Deathlog <- log10(worldDeng$Death)
+
+# COVID2022$location[COVID2022$location == 'United States'] <- 'USA'
+# COVID2022$location[COVID2022$location == 'United Kingdom'] <- 'UK'
+# COVID2022$location[COVID2022$location == 'Democratic Republic of Congo'] <- 'Democratic Republic of the Congo'
+# COVID2022$location[COVID2022$location == 'Congo'] <- 'Republic of Congo'
+
+
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(maps)
+
+options(scipen = 999) ## To disable scientific notation
+world <- map_data("world")
+worldplot <- ggplot() +
+  geom_polygon(data = world, aes(x=long, y = lat, group = group)) + 
+  coord_fixed(1.3)
+worldplot
+
+#Deaths
+worldgovt <- dplyr::select(worldDeng, region = Country, DD = Deathlog)
+head(worldgovt)
+
+
+## Make the HDI numeric
+worldgovt$DC <- as.numeric(as.character(worldgovt$DD))
+worldSubset <- inner_join(world, worldgovt, by = "region")
+head(worldSubset)
+
+
+## Let's ditch many of the unnecessary elements
+plain <- theme(
+  axis.text = element_blank(),
+  axis.line = element_blank(),
+  axis.ticks = element_blank(),
+  panel.border = element_blank(),
+  panel.grid = element_blank(),
+  axis.title = element_blank(),
+  panel.background = element_rect(fill = "white"),
+  plot.title = element_text(hjust = 0.5)
+)
+
+worldDeng <- ggplot(data = worldSubset, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) +
+  geom_polygon(aes(fill = DD)) +
+  scale_fill_distiller(palette ="Reds", direction = 1) + # or direction=1
+  ggtitle("") + labs(fill = "Dengue Deaths \n(log10)") +
+  plain
+y <- plot(worldDeng)
+y
+
+
+library(gridExtra)
+tiff("Dengue2023Map.tiff", units="in", width=20, height=12, res=300)
+gridExtra::grid.arrange(x,y, nrow=1, ncol=2)
+dev.off()
